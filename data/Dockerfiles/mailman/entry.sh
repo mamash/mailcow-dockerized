c8244ea93e5b72e2e1f0cdfe7c95411b7b317136
Filip Hajny <filip@hajny.net>
2019-08-24 16:35:23 +0000

    Initial support for Mailman

---
+++ data/Dockerfiles/mailman/entry.sh
@@ -0,0 +1,64 @@
+#!/usr/bin/env bash
+
+set -e
+
+[ "$DEBUG" == 'true' ] && set -x
+
+# Config checks
+[ -z "$MAILMAN_URLHOST" ] && echo "Error: MAILMAN_URLHOST not set" && exit 128 || true
+[ -z "$MAILMAN_EMAILHOST" ] && echo "Error: MAILMAN_EMAILHOST not set" && exit 128 || true
+[ -z "$MAILMAN_ADMINMAIL" ] && echo "Error: MAILMAN_ADMINMAIL not set" && exit 128 || true
+[ -z "$MAILMAN_ADMINPASS" ] && echo "Error: MAILMAN_ADMINPASS not set" && exit 128 || true
+
+# Copy default mailman etc from cache
+if [ ! "$(ls -A /etc/mailman)" ]; then
+   cp -a /etc/mailman.cache/. /etc/mailman/
+fi
+
+# Copy default mailman data from cache
+if [ ! "$(ls -A /var/lib/mailman)" ]; then
+   cp -a /var/lib/mailman.cache/. /var/lib/mailman/
+fi
+
+# Copy default spool from cache
+if [ ! "$(ls -A /var/spool/postfix)" ]; then
+   cp -a /var/spool/postfix.cache/. /var/spool/postfix/
+fi
+
+# Fix permissions
+/usr/lib/mailman/bin/check_perms -f
+
+# If master list does not exist, create it
+if [ ! -d /var/lib/mailman/lists/mailman ]; then
+    /var/lib/mailman/bin/newlist --quiet --urlhost=$MAILMAN_URLHOST --emailhost=$MAILMAN_EMAILHOST mailman $MAILMAN_ADMINMAIL $MAILMAN_ADMINPASS
+fi
+
+# Set mailman admin password
+mmsitepass $MAILMAN_ADMINPASS
+
+# Fix Postfix settings
+postconf -e myhostname=$MAILMAN_URLHOST
+postconf -e mydestination='$myhostname'
+postconf -e alias_maps=hash:/var/lib/mailman/data/aliases
+postconf -e virtual_alias_maps=hash:/var/lib/mailman/data/virtual-mailman 
+postconf -e virtual_alias_domains=hash:/etc/postfix/virtual
+
+# 20MB size limit
+postconf -e mailbox_size_limit=20971520
+
+if [ -n "$MAILMAN_SSL_CRT" ] && [ -n "$MAILMAN_SSL_KEY" ] && [ -n "$MAILMAN_SSL_CA" ]; then
+    postconf -e smtpd_use_tls=yes
+    postconf -e smtpd_tls_cert_file=${MAILMAN_SSL_CRT}
+    postconf -e smtpd_tls_key_file=${MAILMAN_SSL_KEY}
+    postconf -e smtpd_tls_CAfile=${MAILMAN_SSL_CA}
+fi
+
+# Init Postfix Config
+/usr/lib/mailman/bin/genaliases -q >> /etc/aliases
+newaliases
+
+touch /etc/postfix/virtual
+postmap /etc/postfix/virtual
+
+
+exec $@
