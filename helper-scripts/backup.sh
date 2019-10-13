3de0c777229777da1fad492d879cd4aa8e1e5528
Filip Hajny <filip@hajny.net>
2019-10-13 21:46:28 +0000

    Add backup script

---
+++ helper-scripts/backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+export MAILCOW_BACKUP_LOCATION=/mnt/mailcow/backups
+
+basedir=$(cd $(dirname $0)/..; pwd)
+cd ${basedir}
+
+echo "Running backup"
+#./helper-scripts/backup_and_restore.sh backup all
+
+echo "Syncing to Backblaze"
+docker run --rm \
+  -v ${MAILCOW_BACKUP_LOCATION}:/backups \
+  -v ${basedir}/data/conf/rclone.conf:/root/.rclone.conf \
+  rclone/rclone:latest \
+  sync /backups pysket:/pysket
