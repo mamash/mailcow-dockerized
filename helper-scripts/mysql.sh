0f5a67a602c8fd60d80b661f9d01a4f08149cb65
Filip Hajny <filip@hajny.net>
2019-10-13 21:32:57 +0000

    Add createdb arg to mysql helper

--- helper-scripts/mysql.sh
+++ helper-scripts/mysql.sh
@@ -5,17 +5,24 @@ if [ -z "$1" ]; then
 
 Usage:
 
-  $0 <mysql|import>
+  $0 <createdb|mysql|import> [dbname]
 
 EOF
   exit 1
 fi
 
 cmd=${1}
+db=${2}
 base_dir=$(cd $(dirname $0)/..; pwd)
 . ${base_dir}/.env
 
 case ${cmd} in
+  createdb)
+    [ -z "${db}" ] || (\
+      docker-compose exec mysql-mailcow mysql -p${DBROOT} -e "create database ${db} character set utf8 collate utf8_general_ci"
+      docker-compose exec mysql-mailcow mysql -p${DBROOT} -e "grant all privileges on ${db}.* to ${DBUSER}@'%' identified by '${pass}'"
+      docker-compose exec mysql-mailcow mysql -p${DBROOT} -e "flush privileges")
+    ;;
   mysql)
     docker-compose exec mysql-mailcow mysql -u${DBUSER} -p${DBPASS} ${DBNAME}
     ;;
