70208eecc807e0a863bbb79640543982c164f434
Filip Hajny <filip@hajny.net>
2024-02-17 18:05:21 +0100

    Add MySQL helper script

---
+++ helper-scripts/mysql.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  cat <<EOF
+
+Usage:
+
+  $0 <createdb|mysql|import|dump> [dbname]
+
+EOF
+  exit 1
+fi
+
+cmd=${1}
+db=${2}
+base_dir=$(cd $(dirname $0)/..; pwd)
+. ${base_dir}/.env
+
+case ${cmd} in
+  createdb)
+    [ -z "${db}" ] || (\
+      docker-compose exec mysql-mailcow mysql -p${DBROOT} -e "create database ${db} character set utf8 collate utf8_general_ci"
+      docker-compose exec mysql-mailcow mysql -p${DBROOT} -e "grant all privileges on ${db}.* to ${DBUSER}@'%' identified by '${pass}'"
+      docker-compose exec mysql-mailcow mysql -p${DBROOT} -e "flush privileges")
+    ;;
+  dump)
+    docker-compose exec -T mysql-mailcow mysqldump -u${DBUSER} -p${DBPASS} ${db}
+    ;;
+  mysql)
+    docker-compose exec mysql-mailcow mysql -u${DBUSER} -p${DBPASS} ${DBNAME}
+    ;;
+  import)
+    docker-compose exec -T mysql-mailcow mysql -u${DBUSER} -p${DBPASS} ${DBNAME}
+    ;;
+esac
